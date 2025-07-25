3d8be0434d8e19605ad2e0acce53187e0745f89c
Aryan Patel <arypat2005@gmail.com>
2025-07-24 22:51:51 -0400

    Changed Footer

--- Podcast/src/app/components/Footer.js
+++ Podcast/src/app/components/Footer.js
@@ -1,12 +1,37 @@
+import { FaLinkedin } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+
 export default function Footer() {
   return (
     <footer className="mt-auto bg-slate-900 border-t border-slate-700">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <div className="text-center">
-          <p className="text-white font-medium">&copy; 2025 AWS Podverse. Built with Next.js and AWS.</p>
-          <p className="text-slate-400 mt-2">By Aryan Patel</p>
+        <div className="text-center mb-4">
+          <p className="text-white font-medium">
+            &copy; 2025 AWS Podverse. Built with Next.js and AWS.
+          </p>
+        </div>
+
+        <div className="flex items-center justify-center items-center mx-auto max-w-xs text-slate-400">
+          <div className="flex space-x-4">
+            <a
+              href="https://github.com/ARYPAT2005"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-white transition"
+            >
+              <FaGithub size={20} />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/aryan-patel-76b87a279/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-white transition"
+            >
+              <FaLinkedin size={20} />
+            </a>
+          </div>
         </div>
       </div>
     </footer>
-  )
+  );
 }
