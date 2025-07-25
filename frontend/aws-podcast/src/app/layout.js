7ec44b159b18d85167d9e12e2e22d92715fdfb6e
Aryan Patel <arypat2005@gmail.com>
2025-07-24 20:43:20 -0400

    Changed favicon icon

--- frontend/aws-podcast/src/app/layout.js
+++ frontend/aws-podcast/src/app/layout.js
@@ -1,35 +1,36 @@
-"use client"
+// app/layout.js
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header.js";
-import Footer from "./components/Footer.js";
-import { ThemeProvider } from "./components/theme-provider.js";
-import { useAuthStore } from "@/store/authstore";
-import { useEffect } from "react"
-import { jwtDecode } from 'jwt-decode';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { ThemeProvider } from "./components/theme-provider";
+import AuthChecker from "./components/AuthChecker";
 
+export const metadata = {
+  icons: {
+    icon: '/image.png',
+  },
+};
 
 export default function RootLayout({ children }) {
-  const { checkAuth } = useAuthStore();
-  useEffect(() => {
-    checkAuth()
-  },[checkAuth])
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/image.png" />
+      </head>
       <body>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
-        >   
-            <div className="min-h-screen flex flex-col">
-              <Header />
-              <main className="flex-1">
-                {children}
-              </main>
-              <Footer />
-            </div>
+        >
+          <AuthChecker />
+          <div className="min-h-screen flex flex-col">
+            <Header />
+            <main className="flex-1">{children}</main>
+            <Footer />
+          </div>
         </ThemeProvider>
       </body>
     </html>
